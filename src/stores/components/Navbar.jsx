2313b24349f7708ff21e8d98082bbb7ac21983b8
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2025-02-01 09:20:02 +0530

    Update Navbar.jsx

--- src/stores/components/Navbar.jsx
+++ src/stores/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useCart } from '../context/CartContext'
 
 function Navbar(){
     const {cartItems }= useCart()
+
+    const [showmore,setshowmore] = useState(false);
+
     return (
         <>
         <div className='navsection'>
@@ -31,15 +34,28 @@ function Navbar(){
                     <Link to = "/mobiles" className="custom-link"><li>Mobiles</li></Link>
                     <Link to = "/Computers" className="custom-link"><li>Computers</li></Link>
                     <Link to = "/Watches" className="custom-link"><li>Watches</li></Link>
-                    <Link to = "/Menswear" className="custom-link"><li>Mens Wear</li></Link>
-                    <Link to = "/Womenswear" className="custom-link"><li>Womens Wear</li></Link>
-                    <Link to = "/furniture" className="custom-link"><li>Furniture</li></Link>
-                    <Link to = "/kitchen" className="custom-link"><li>Kitchen</li></Link>
+                    <Link to = "/TVs" className="custom-link"><li>TV's</li></Link>
                     <Link to = "/acs" className="custom-link"><li>Ac's</li></Link>
                     <Link to = "/fridge" className="custom-link"><li>Fridge</li></Link>
-                    <Link to = "/TVs" className="custom-link"><li>TV's</li></Link>
-                    <Link to = "/books" className="custom-link"><li>Books</li></Link>
                     <Link to = "/Speaker" className="custom-link"><li>Speaker</li></Link>
+                    
+
+                    <li className='more-menu' onMouseEnter={()=> setshowmore(true)} onMouseLeave={()=> setshowmore(false)}>
+                    More ▾ 
+                        {
+                            showmore && (
+                                <ul className='dropdown'> 
+                                <Link to = "/Menswear" className="custom-link"><li>Mens Wear</li></Link>
+                                <Link to = "/Womenswear" className="custom-link"><li>Womens Wear</li></Link>
+                                <Link to = "/furniture" className="custom-link"><li>Furniture</li></Link>
+                                <Link to = "/kitchen" className="custom-link"><li>Kitchen</li></Link>
+                                <Link to = "/books" className="custom-link"><li>Books</li></Link>
+                                </ul>
+                            )
+                        }
+
+                    </li>
+                    
                 </ul>
             </div>
         </>
