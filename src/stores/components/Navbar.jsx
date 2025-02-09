bb131bfd8e09aa5cd969b6dd99a53d814e1f75cc
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2025-02-09 11:50:06 +0530

    Update Navbar.jsx

--- src/stores/components/Navbar.jsx
+++ src/stores/components/Navbar.jsx
@@ -1,64 +1,61 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { useCart } from '../context/CartContext'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import {FaUser} from 'react-icons/fa';
 
-function Navbar(){
-    const {cartItems }= useCart()
 
-    const [showmore,setshowmore] = useState(false);
+function Navbar() {
+    const { cartItems } = useCart();
+    const [showMore, setShowMore] = useState(false);
 
     return (
         <>
-        <div className='navsection'>
-            <div className="title"> 
-                <h2>E-Mart</h2>
-            </div>
-            <div class="search-container">
-                 <input type="text" placeholder='search..' className='search-input'/>
-            </div>
-            <div className="user">
-                    SignIn/SignUp
-            </div>
-            <Link to = "/cart" className="custom-link">
-                <div className="Cart">
-                    Cart 
-                    <span>
-                        {cartItems.length}
-                    </span>
+            <div className="navsection">
+                <div className="title">
+                    <h2>E-Mart</h2>
                 </div>
-            </Link>
-        </div>
-        <div className="subMenu">
+                <div className="search-container">
+                    <input type="text" placeholder="Search..." className="search-input" />
+                </div>
+                <div className="user">
+                    <FaUser/>
+                </div>
+                <Link to="/cart" className="custom-link">
+                    <div className="Cart">
+                        Cart <span>{cartItems.length}</span>
+                    </div>
+                </Link>
+            </div>
+
+            {/* Submenu with horizontal scrolling on mobile */}
+            <div className="subMenu">
                 <ul>
-                    <Link to = "/" className='custom-link'><li>Home</li></Link>
-                    <Link to = "/mobiles" className="custom-link"><li>Mobiles</li></Link>
-                    <Link to = "/Computers" className="custom-link"><li>Computers</li></Link>
-                    <Link to = "/Watches" className="custom-link"><li>Watches</li></Link>
-                    <Link to = "/TVs" className="custom-link"><li>TV's</li></Link>
-                    <Link to = "/acs" className="custom-link"><li>Ac's</li></Link>
-                    <Link to = "/fridge" className="custom-link"><li>Fridge</li></Link>
-                    <Link to = "/Speaker" className="custom-link"><li>Speaker</li></Link>
-                    
+                    <Link to="/" className="custom-link"><li>Home</li></Link>
+                    <Link to="/mobiles" className="custom-link"><li>Mobiles</li></Link>
+                    <Link to="/computers" className="custom-link"><li>Computers</li></Link>
+                    <Link to="/watches" className="custom-link"><li>Watches</li></Link>
+                    <Link to="/tvs" className="custom-link"><li>TVs</li></Link>
+                    <Link to="/acs" className="custom-link"><li>ACs</li></Link>
+                    <Link to="/fridge" className="custom-link"><li>Fridge</li></Link>
+                    <Link to="/speaker" className="custom-link"><li>Speaker</li></Link>
 
-                    <li className='more-menu' onMouseEnter={()=> setshowmore(true)} onMouseLeave={()=> setshowmore(false)}>
-                    More ▾ 
-                        {
-                            showmore && (
-                                <ul className='dropdown'> 
-                                <Link to = "/Menswear" className="custom-link"><li>Mens Wear</li></Link>
-                                <Link to = "/Womenswear" className="custom-link"><li>Womens Wear</li></Link>
-                                <Link to = "/furniture" className="custom-link"><li>Furniture</li></Link>
-                                <Link to = "/kitchen" className="custom-link"><li>Kitchen</li></Link>
-                                <Link to = "/books" className="custom-link"><li>Books</li></Link>
-                                </ul>
-                            )
-                        }
-
+                    {/* Dropdown for More */}
+                    <li className="more-menu" onClick={() => setShowMore(!showMore)}>
+                        More ▾
+                        {showMore && (
+                            <ul className="dropdown">
+                                <Link to="/menswear" className="custom-link"><li>Menswear</li></Link>
+                                <Link to="/womenswear" className="custom-link"><li>Womenswear</li></Link>
+                                <Link to="/furniture" className="custom-link"><li>Furniture</li></Link>
+                                <Link to="/kitchen" className="custom-link"><li>Kitchen</li></Link>
+                                <Link to="/books" className="custom-link"><li>Books</li></Link>
+                            </ul>
+                        )}
                     </li>
-                    
                 </ul>
             </div>
         </>
-    )
+    );
 }
-export default Navbar
+
+export default Navbar;
